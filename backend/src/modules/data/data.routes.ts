979e067ee091eff47eb63eb6403698396e57538e
Hailegebriel <hailegebrielbekalu47@gmail.com>
2026-01-23 11:13:51 +0300

    Add src/modules/data/data.routes.ts

---
+++ backend/src/modules/data/data.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { dataController } from "./data.controller";
+import { upload } from "../../config/multer";
+
+const router = Router();
+
+router.post("/:projectId/upload",upload.single("file"),dataController.uploadFile)
+router.post("/:projectId/addTextData",dataController.addTextData)
+router.post("/:projectId/add-missing-data",dataController.addMissingData)
+
+export default router
