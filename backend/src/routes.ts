e35a871a7244bf4f62cad1798bfc4f7dba21dd35
Natnael Esayas <Nanoboss44@gmail.com>
2026-01-23 19:48:51 +0300

    updated routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 //import { authMiddleware } from "./middlewares/auth.middleware";
 import projectRoute from "./modules/projects/projects.routes"
+import taskRoute from "./modules/tasks/tasks.routes"
 import fileRoute from "./modules/data/data.routes"
 import assistant from "./modules/assistant/assistant.routes"
 
@@ -10,5 +11,5 @@ export const router = Router();
 router.use("/projects", projectRoute)
 router.use("/project", fileRoute)
 router.use("/assistant", assistant)
-
+router.use("/projects/asks", taskRoute)
 export default router
