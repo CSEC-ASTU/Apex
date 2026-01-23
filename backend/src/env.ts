b5496d54ff04448bb05af23002276be1e40679c6
Hailegebriel <hailegebrielbekalu47@gmail.com>
2026-01-23 11:14:43 +0300

    env configuration added

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -4,14 +4,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const EnvSchema = z.object({
-  DATABASE_URL: z.string().url(),
-  PORT: z.string().default("5000"),
-  JWT_SECRET: z.string("hello_every_one!"),
-  BASE_URL: z.string().default("http://localhost:5000"),
-  NODE_ENV: z.string().default("development"),
-  JWT_ACCESS_SECRET: z.string().default("access_secret"),
-  JWT_REFRESH_SECRET: z.string().default("secret_secret"),
-  GEMINI_API_KEY: z.string(),
+    DATABASE_URL: z.string().url(),
+    PORT: z.string().default("5000"),
+    JWT_SECRET: z.string("hello_every_one!"),
+    BASE_URL: z.string().default("http://localhost:5000"),
+    NODE_ENV: z.string().default("development"),
+    JWT_ACCESS_SECRET: z.string().default("access_secret"),
+    JWT_REFRESH_SECRET: z.string().default("secret_secret"),
+    GEMINI_API_KEY: z.string(),
+    CORS_ORIGIN: z.string().default("http://localhost:5173"),
+
 });
 
 export const env = EnvSchema.parse(process.env);
